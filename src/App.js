e6bce9801902fe9691f69b82b6bce0971e5425e8
Bruno Pereira <brunno.pereira7@gmail.com>
2022-05-24 20:57:57 -0400

    Create base app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@mui/material";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+export default function App () {
+  const notify = () => toast("Wow so easy !");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer autoClose={8000} style={{ width: "20em" }}/>
+      <Button onClick={notify} variant="contained"> Olá Mundo</Button>
+    </>
   );
 }
-
-export default App;
