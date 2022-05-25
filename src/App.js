65152239a533723c046fce3fabcaffa6bb07cf65
Bruno Pereira <brunno.pereira7@gmail.com>
2022-05-24 23:23:35 -0400

    add theme context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './Routes';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ptBR } from '@mui/material/locale';
+import { amber, blue, grey, red } from '@mui/material/colors';
 
-export default function App () {
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: blue[700] },
+      secondary: { main: "#d28e19" },
+      divider: amber[200],
+      danger: red.A700,
+      text: {
+        primary: grey[900],
+        secondary: grey[800],
+      },
+    },
+  },
+  ptBR,
+);
+
+export default function App() {
   return (
     <>
-      <BrowserRouter>
-        <ToastContainer autoClose={8000} style={{ width: "20em" }}/>
-        <Routes />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <ToastContainer autoClose={8000} style={{ width: "20em" }} />
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
