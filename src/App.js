02ced5d19341095aad88dd1a1bebf4af89fc6630
Bruno Pereira <brunno.pereira7@gmail.com>
2022-05-24 22:07:00 -0400

    create simple routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Button } from "@mui/material";
-import { toast, ToastContainer } from 'react-toastify';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from './Routes';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App () {
-  const notify = () => toast("Wow so easy !");
   return (
     <>
-      <ToastContainer autoClose={8000} style={{ width: "20em" }}/>
-      <Button onClick={notify} variant="contained"> Olá Mundo</Button>
+      <BrowserRouter>
+        <ToastContainer autoClose={8000} style={{ width: "20em" }}/>
+        <Routes />
+      </BrowserRouter>
     </>
   );
 }
