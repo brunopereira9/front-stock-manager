8bef1884bee55a8f194b7a567b01fb039bcfe3c4
Bruno Pereira <brunno.pereira7@gmail.com>
2022-05-25 22:21:37 -0400

    ADD TODO

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import { SearchBar } from '../Components/Product/SearchBar';
 import { TableView } from '../Components/Product/TableView';
 
 export const Home = () => {
+  // TODO: criar busca de produtos, listagem das alterações de estoque, gráfico de variação de preço
   return (
     <>
       <NavBar />
